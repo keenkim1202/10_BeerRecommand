68379882ee4e0533e7a037782ad344d41ee74a7d
KEEN <homurahomu@naver.com>
2021-12-21 15:09:48 +0900

    project init

---
+++ BeerRecommand/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeerRecommand
+//
+//  Created by KEEN on 2021/12/21.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+}
+
